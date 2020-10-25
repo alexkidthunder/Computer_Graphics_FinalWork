17cdce59f8ce73a3535f0368bf9f8427a8ae1142
alexkidthunder <ribeiroo.grande@gmail.com>
2020-10-25 15:00:40 -0300

    Loop Music
    Add music loop e remove some bugs

--- NewSpaceShooter/game.cpp
+++ NewSpaceShooter/game.cpp
@@ -4,6 +4,7 @@
 #include <QFont>
 #include "enemy.h"
 #include <QMediaPlayer>
+#include <QMediaPlaylist>
 
 Game::Game(QWidget *parent){
     // create the scene
@@ -43,11 +44,17 @@ Game::Game(QWidget *parent){
     QObject::connect(timer,SIGNAL(timeout()),player,SLOT(spawn()));
     timer->start(2000);
 
-    // play background music
+    // Play background music
+
+    QMediaPlaylist *playlist = new QMediaPlaylist();//Loop
+    playlist->addMedia(QUrl("qrc:/bg.wav"));
+    playlist->setPlaybackMode(QMediaPlaylist::Loop);
+
     QMediaPlayer * music = new QMediaPlayer();
-    music->setMedia(QUrl("qrc:/bg.wav"));
+    //music->setMedia(QUrl("qrc:/bg.wav"));// Single Play
+    music->setPlaylist(playlist);// Config Loop
     music->setVolume(26);
-    music->play();
+    music->play(); // Play
 
     show();
 }
